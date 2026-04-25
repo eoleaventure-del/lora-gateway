ea478214413f4f9b8796d768e7d5f9f0d962c003
LORIN Olivier <olivier@MacBook-Pro-Olivier.local>
2026-04-25 13:12:06 +0200

    Debug: disable station start

--- lora-gateway/entrypoint.sh
+++ lora-gateway/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-if [ ! -f /data/station.conf ]; then
-  echo "Init station.conf"
-  cp /opt/station.conf /data/station.conf
-fi
+set -e
 
-cd /data
-exec /opt/basicstation/build-rpi-std/bin/station
+echo "DEBUG MODE - container stays alive"
+
+sleep infinity
